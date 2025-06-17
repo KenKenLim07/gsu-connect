99eadf4500043fdebd87be61a923da2e3dc98e6c
KenKenLim07 <josemarielim7@gmail.com>
2025-06-17 22:53:40 +0800

    Updates

---
+++ assets/supabaseClient-BVR6Ckat.js
@@ -0,0 +1,1 @@
+import{c as s}from"./index-DbMEnAJT.js";const I="https://fkyokvjruvgsritmazey.supabase.co",c="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZreW9rdmpydXZnc3JpdG1hemV5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTMwNTc1MywiZXhwIjoyMDY0ODgxNzUzfQ.2s8Wpf_IjYG5OKL0w7BsPe6k5NIm2kYO6IH0MY8uLgE",p=s(I,c);export{p as s};
