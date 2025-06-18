636af4f2270d00da8ee2ce1c66cba12fbcb81272
KenKenLim07 <josemarielim7@gmail.com>
2025-06-19 03:29:47 +0800

    Updates

--- assets/supabaseClient-Dzld-KKh.js
+++
@@ -1,2 +0,0 @@
-import{c as s}from"./index-BAYP90Ju.js";const I="https://fkyokvjruvgsritmazey.supabase.co",c="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZreW9rdmpydXZnc3JpdG1hemV5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTMwNTc1MywiZXhwIjoyMDY0ODgxNzUzfQ.2s8Wpf_IjYG5OKL0w7BsPe6k5NIm2kYO6IH0MY8uLgE",p=s(I,c);export{p as s};
-//# sourceMappingURL=supabaseClient-Dzld-KKh.js.map
